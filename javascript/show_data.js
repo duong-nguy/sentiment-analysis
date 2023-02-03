2a579acc4d4df6a52b4d9eb41e7eb8a9decae1b0
duong-nguy <chanduong1999@gmail.com>
2023-02-03 19:36:30 +0100

    edit and remove

--- javascript/show_data.js
+++ javascript/show_data.js
@@ -9,6 +9,49 @@ document.getElementById("next-btn").addEventListener("click", () => {
     request_data();
 });
 function request_data() {
+    request = "off-set=" + encodeURIComponent(off_set) +
+        "&n-rows=" + encodeURIComponent(N_ROWS);
+    send_request(request);
+}
+function request_detele_row() {
+    id = this.parentNode.parentNode.cells[0].innerHTML;
+    request =
+        "off-set=" + encodeURIComponent(off_set) +
+        "&n-rows=" + encodeURIComponent(N_ROWS) +
+        "&delete=" + encodeURIComponent(id);
+    send_request(request);
+}
+function request_edit_row() {
+    id = this.parentNode.parentNode.cells[0].innerHTML;
+    row = this.parentNode.parentNode;
+    new_row = id + ",";
+    for (let index = 1; index < row.cells.length - 2; index++) {
+        const cell = row.cells[index];
+        cell_value = cell
+            .querySelector(`#${cell.id}`)
+            .value;
+        if (cell_value == "") {
+            document.getElementById("feedback-msg").innerHTML =
+                `${cell.id} is empty`;
+            return;
+        };
+        if (row.cells[index].id == "age" && !Number.isFinite(cell_value)) {
+            document.getElementById("feedback-msg").innerHTML =
+                `${cell.id} can only not be number`;
+            return;
+        }
+        new_row += cell_value + ",";
+    }
+    new_row = new_row.substring(0, new_row.length - 1);
+    request =
+        "off-set=" + encodeURIComponent(off_set) +
+        "&n-rows=" + encodeURIComponent(N_ROWS) +
+        "&edit=" + encodeURIComponent(id) +
+        "&new-row=" + encodeURIComponent(new_row);
+    send_request(request);
+    document.getElementById("feedback-msg").innerHTML = "";
+}
+function send_request(request) {
     let xhr = new XMLHttpRequest();
     xhr.open("POST", "php/read_data.php", true);
     xhr.setRequestHeader("Content-Type",
@@ -19,16 +62,30 @@ function request_data() {
             create_table(xhr.responseText);
         }
     }
-    request = "off-set=" + encodeURIComponent(off_set) +
-        "&n-rows=" + encodeURIComponent(N_ROWS);
     xhr.send(request);
 }
-function request_detele_row(){
-    row = this.parentNode;
-    id = row.getElementByTag("td")[0];
-    console.log(id.innerHTML);
+function create_edit_fields() {
+    row = this.parentNode.parentNode;
+    id = row.cells[0].innerHTML;
+    for (let index = 1; index < row.cells.length - 2; index++) {
+        cell = row.cells[index];
+        cell.innerHTML = "";
+        text_area = document.createElement("textarea");
+        text_area.id = cell.id;
+        cell.appendChild(text_area);
+    }
+    row.cells[row.cells.length - 1].innerHTML = "";
+    row.cells[row.cells.length - 2].innerHTML = "";
+    return_btn = create_btn("return", request_data)
+    done_btn = create_btn("done", request_edit_row)
+    row.cells[row.cells.length - 1].appendChild(return_btn);
+    row.cells[row.cells.length - 2].appendChild(done_btn);
 }
-function request_edit_row(){
+function create_btn(name, event_lis) {
+    btn = document.createElement("button");
+    btn.innerHTML = name;
+    btn.addEventListener("click", event_lis);
+    return btn;
 }
 function end_of_file(responseText) {
     if (responseText === "[]") {
@@ -36,19 +93,6 @@ function end_of_file(responseText) {
         return true;
     }
     return false;
-}
-function create_btn() {
-    btn_delete = document.createElement("button");
-    btn_edit = document.createElement("button");
-    btn_delete.innerHTML = "Delete";
-    btn_edit.innerHTML = "Edit";
-    btn_delete.classList.add("delete-btn");
-    btn_delete.classList.add("shy-btn");
-    btn_edit.classList.add("delete-btn");
-    btn_edit.classList.add("edit-btn");
-    btn_delete.addEventListener("click",request_detele_row);
-    btn_edit.addEventListener("click",request_edit_row);
-    return [btn_delete,btn_edit];
 }
 function create_table(responseText) {
     if (end_of_file(responseText)) return;
@@ -65,11 +109,14 @@ function create_table(responseText) {
         for (const cell_idx in row_data) {
             if (Object.hasOwnProperty.call(row_data, cell_idx)) {
                 cell = row.insertCell();
+                cell.id = cell_idx;
                 cell.innerHTML = row_data[cell_idx];
             }
         }
-        btn=create_btn()[0];
-        row.insertCell().appendChild(btn);
+        row.insertCell().appendChild(
+            create_btn("delete", request_detele_row));
+        row.insertCell().appendChild(
+            create_btn("edit", create_edit_fields));
     });
 }
 request_data();
